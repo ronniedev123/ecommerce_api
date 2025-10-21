50c0ebaa83910bdc6478e785c992ebd6ccc95c66
ronniedev123 <rontechconsultants@gmail.com>
2025-10-21 12:59:30 +0300

    Add custom User model, registration and profile endpoints

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import User
+from .serializers import RegisterSerializer, UserSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ProfileView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
 
 # Create your views here.
